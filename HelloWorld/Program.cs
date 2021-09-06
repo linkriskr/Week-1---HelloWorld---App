76bf63c3a5b864fae279458e0f0360534e1ff0a2
krislin.kruus <krkruus@tktk.ee>
2021-09-06 15:30:02 +0300

    Create Hello World Program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string UserName;
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + UserName);
+
+            Console.Read();
+
+        }
+    }
+}
